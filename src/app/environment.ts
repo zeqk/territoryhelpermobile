b8d6bb1b344684d3b2353985dd46ac652e571f5f
zeqk <zeqk.net@gmail.com>
2017-11-26 21:50:45 -0300

    environment

---
+++ src/app/environment.ts
@@ -0,0 +1,6 @@
+const environment = {
+    clientId: '37b1ef7e45d447efaa3a85208d2f9b8f',
+    clientSecret: '',
+    domain: 'dev.territoryhelper.com/api',
+    packageIdentifier: 'io.ionic.territoryhelpermobile',
+};
